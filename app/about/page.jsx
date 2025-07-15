e8967e695f16033b4c38e1c8eab92cea9e30dd1b
fdbertolo <fdbertolo@gmail.com>
2025-07-15 15:53:48 -0300

    adding some details

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,6 @@
 import { Markdown } from 'components/markdown';
 import { Footer } from 'components/footer';
+import Button from 'components/button';
 import TiltedCard from 'components/tilted-card';
 
 export const metadata = {
@@ -19,32 +20,35 @@ export default async function Page() {
                 <div className="flex w-full">
                     <div className="flex flex-col gap-2">
                         <h3>When I&apos;m working</h3>
-                        <Markdown content={explainer} className="mb-6" />
+                        <Markdown content={explainer} className="mb-4" />
+                        <a href="/fernando-bertolo-resume.pdf" download="fernando-bertolo-resume.pdf" className='mb-16'>
+                            <Button text="Download my resume" />
+                        </a>
                         <h3>Beyond work!</h3>
                         <Markdown content={explainer2} className="mb-6" />
-                        <iframe
-                            src="https://open.spotify.com/embed/playlist/0h2P5zJxWy9eCMHg9a6qeJ?utm_source=generator&theme=0"
-                            width="100%"
-                            height="150px"
-                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                            loading="lazy"
-                        ></iframe>
                     </div>
                 </div>
-                <div className="flex w-full justify-center">
+                <div className="flex w-full justify-center flex-col items-center gap-4">
                     <TiltedCard
                         imageSrc="/images/back.png"
                         altText="Picture of Fernando Bertolo"
-                        containerHeight="500px"
-                        containerWidth="400px"
-                        imageHeight="500px"
-                        imageWidth="400px"
+                        containerHeight="400px"
+                        containerWidth="350px"
+                        imageHeight="400px"
+                        imageWidth="350px"
                         rotateAmplitude={12}
                         scaleOnHover={1.2}
                         showMobileWarning={false}
                         showTooltip={true}
                         displayOverlayContent={true}
                     />
+                    <iframe
+                        src="https://open.spotify.com/embed/playlist/0h2P5zJxWy9eCMHg9a6qeJ?utm_source=generator&theme=0"
+                        width="350px"
+                        height="400px"
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        loading="lazy"
+                    ></iframe>
                 </div>
             </div>
             <Footer />
